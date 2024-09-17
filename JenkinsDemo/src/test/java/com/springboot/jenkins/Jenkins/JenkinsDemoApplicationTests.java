6ff9fcb6ccaf474cadaa654ef7e3318fbcb7f977
unknown <adepuganesh28@gmail.com>
2024-09-17 14:14:18 +0530

    app

---
+++ JenkinsDemo/src/test/java/com/springboot/jenkins/Jenkins/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jenkins.Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
